2ea5b043b537fb734d5d4e5138a0899e3251d5d9
enrique palomino <octepc.97@gmail.com>
2023-01-14 15:42:25 -0500

    installed matplotlib

---
+++ charts/charts.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+
+def generate_pie_chart():
+    labels = ['A', 'B', 'C']
+    values = [200, 35, 67]
+
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+
+    plt.savefig('pie.png')
+    plt.close
